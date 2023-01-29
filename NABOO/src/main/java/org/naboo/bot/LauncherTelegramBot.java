ab1058d4f78afe6d1aadf94bcf14fe27abd2620e
RedKyrie02 <tommyred2002@gmail.com>
2023-01-29 18:53:40 +0100

    Creazione file .jar

---
+++ NABOO/src/main/java/org/naboo/bot/LauncherTelegramBot.java
@@ -0,0 +1,10 @@
+package org.naboo.bot;
+
+import org.naboo.pannelloFx.MainJFx;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class LauncherTelegramBot {
+    public static void main(String args[]) throws TelegramApiException {
+        TelegramLoader.main(args);
+    }
+}
---
+++ NABOO/src/main/java/org/naboo/pannelloFx/LauncherPannelloGrafico.java
@@ -0,0 +1,7 @@
+package org.naboo.pannelloFx;
+
+public class LauncherPannelloGrafico {
+    public static void main(String args[]){
+        MainJFx.main(args);
+    }
+}
